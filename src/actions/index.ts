30fa4bc716075dff056e27356537bef1f52a3fd4
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-06-26 10:38:25 -0300

    git push

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,13 +5,11 @@ import { CosmicObject } from '@/lib/cosmic-types'
 export async function getCosmicData(): Promise<CosmicObject> {
   const apiURL = String(process.env.API_URL)
 
-  const res = await fetch(apiURL, {
+  return fetch(apiURL, {
     method: 'GET',
     next: {
       revalidate: 86400,
       tags: ['cosmic-object'],
     },
-  })
-
-  return res.json()
+  }).then((res) => res.json())
 }
