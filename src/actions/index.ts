8eb513c267ca7f86a94c62a0df209bf81d3b0a3e
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-06-26 10:29:44 -0300

    add enviroment variable

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,16 +3,15 @@
 import { CosmicObject } from '@/lib/cosmic-types'
 
 export async function getCosmicData(): Promise<CosmicObject> {
-  const res = await fetch(
-    'https://api.cosmicjs.com/v3/buckets/autogestor-production/objects/667b5d5a864193998e15e9f7?read_key=UYSFgLvlR2SsD3ICdReucyKSlsEy71lABNMeAEMNFVQVXbRc92&depth=1&props=metadata,',
-    {
-      method: 'GET',
-      next: {
-        revalidate: 86400,
-        tags: ['cosmic-object'],
-      },
+  const apiURL = String(process.env.API_URL)
+
+  const res = await fetch(apiURL, {
+    method: 'GET',
+    next: {
+      revalidate: 86400,
+      tags: ['cosmic-object'],
     },
-  )
+  })
 
   return res.json()
 }
